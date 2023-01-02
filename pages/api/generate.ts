18eba8f55de0b9c6d2e4dd1617c894812448e1d2
Ian <github@ianc.me>
2023-01-02 16:37:01 -0500

    feat: basic writer

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,4 +1,35 @@
+import { NextApiHandler } from "next"
 import { Configuration, OpenAIApi } from "openai"
+
+function parseEdits(text: string) {
+  const edits = text.split("\n")
+  let currentEdit = 0
+  const parsedEdits = []
+
+  for (const line of edits) {
+    console.log(line, currentEdit)
+    if (line.startsWith("Edit #")) {
+      currentEdit = parseInt(line.match(/Edit #(\d+):/)[1])
+      parsedEdits[currentEdit] = {}
+      const textWithTrailingQuote = line.split("Original Text - ")[1]
+      const lastQuoteIndex = textWithTrailingQuote.lastIndexOf(`"`)
+      const firstQuoteIndex = textWithTrailingQuote.indexOf(`"`)
+      parsedEdits[currentEdit].original = textWithTrailingQuote.slice(
+        firstQuoteIndex + 1,
+        lastQuoteIndex
+      )
+    }
+    if (line.startsWith("Edited Text")) {
+      const textWithTrailingQuote = line.split('Edited Text - "')[1]
+      const quoteIndex = textWithTrailingQuote.lastIndexOf(`"`)
+      parsedEdits[currentEdit].edit = textWithTrailingQuote.slice(0, quoteIndex)
+    }
+    if (line.startsWith("Suggestion:")) {
+      parsedEdits[currentEdit].suggestion = line.split("Suggestion: ")[1]
+    }
+  }
+  return parsedEdits.filter((edit) => edit)
+}
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -6,21 +37,115 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration)
 
-const basePromptPrefix = ""
-const generateAction = async (req, res) => {
+const basePromptPrefix = (prompt: string, text: string) => `
+Prompt: Rewrite this text to use figurative language. Do not edit quotes. After writing the edit, list each edit made, including the original text, the edited text, and a detailed reason for editing. Write your reasons as suggestions (e.g., "Consider changing <something> in order to make it more interesting). You MUST list EVERY edit made to the text. List EVERY edit made to the text, in order.
+
+Original Text:
+Farewelling is all about celebrating a beautiful life, beautifully. Knowing how to write a eulogy is a part of keeping that person’s legacy alive and well. Write it with gusto. Deliver it with love and respect. And above all, know that whatever you write doesn’t have to be perfect. To craft a truly great eulogy, just follow the guidelines above. Combine a bit of structure with a dash of personal style, and you'll have a fitting tribute to share with friends and family.
+
+Write out the edited text, followed by the list of edits:
+
+Edited Text:
+
+Farewelling is an opportunity to honour a life, masterfully. Crafting a eulogy is a way to preserve their memory and keep their legacy alive. Compose it with vigor. Relay it with reverence. Above all, accept that whatever you write doesn’t need to be flawless. To put together a truly remarkable eulogy, just abide by the rules above. Blend a pinch of structure with a sprinkle of individual flair, and you'll have a fitting tribute to share with loved ones.
+
+Edit #1: Original Text - "is all about celebrating a beautiful life, beautifully."
+Edited Text - "is an opportunity to honour a life, masterfully"
+Suggestion: Consider using "opportunity" to change the tone and using "masterfully" to use more advanced language.
+
+Edit #1: Original Text - "Knowing how to write a eulogy is a part of keeping that person’s legacy alive and well" 
+Edited Text - "Crafting a eulogy is a way to preserve their memory and keep their legacy alive" 
+Suggestion: Consider changing "write a eulogy" to "craft a eulogy" in order to give it a more personal touch. 
+
+Edit #2: Original Text - "Write it with gusto" 
+Edited Text - "Compose it with vigor" 
+Suggestion: Consider changing "write it" to "compose it" in order to make it more poetic.
+
+Edit #3: Original Text - "And above all, know that whatever you write doesn’t have to be perfect" 
+Edited Text - "Above all, accept that whatever you write doesn’t need to be flawless" 
+Suggestion: Consider changing "have to be perfect" to "need to be flawless" in order to make it sound more confident, and removing the redundant "And".
+
+Edit #4: Original Text - "To craft a truly great eulogy, just follow the guidelines above" 
+Edited Text - "To put together a truly remarkable eulogy, just abide by the rules above" 
+Suggestion: Consider changing "craft a truly great eulogy" to "put together a truly remarkable eulogy" in order to make it sound more compelling.
+
+Edit #5: Original Text - "Combine a bit of structure with a dash of personal style" 
+Edited Text - "Blend a pinch of structure with a sprinkle of individual flair" 
+Suggestion: Consider changing "combine a bit of structure" to "blend a pinch of structure" and "a dash of personal style" to "a sprinkle of individual flair" in order to make it sound more delicate.
+
+Edit #6: Original Text -  "tribute to share with friends and family" 
+Edited Text - "tribute to share with loved ones" 
+Suggestion: Consider changing "friends and family" to "loved ones" in order to make it sound more personal.
+
+END
+
+Prompt: Rewrite this text to use figurative language. Do not edit quotes. After writing the edit, list each edit made, including the original text, the edited text, and a detailed reason for editing. Write your reasons as suggestions (e.g., "Consider changing <something> in order to make it more interesting). You MUST list EVERY edit made to the text. List EVERY edit made to the text, in order.
+
+Original Text:
+Thiel begins with the contrarian premise that we live in an age of technological stagnation, even if we’re too distracted by shiny mobile devices to notice. Information technology has improved rapidly, but there is no reason why progress should be limited to computers or Silicon Valley. Progress can be achieved in any industry or area of business. It comes from the most important skill that every leader must master: learning to think for yourself.
+
+Doing what someone else already knows how to do takes the world from 1 to n, adding more of something familiar. But when you do something new, you go from 0 to 1. The next Bill Gates will not build an operating system. The next Larry Page or Sergey Brin won’t make a search engine. Tomorrow’s champions will not win by competing ruthlessly in today’s marketplace. They will escape competition altogether, because their businesses will be unique.
+
+Write out the edited text, followed by the list of edits:
+
+Edited Text: 
+Thiel launches with the contrary notion that we exist in an era of technological monotony, even if we’re too preoccupied by glimmering electronic devices to remember. Information technology has progressed rapidly, yet there is no justification why success should be restricted to computers or Silicon Valley. Advancement can be accomplished in any sector or region of industry. It comes from the most crucial ability that each leader must master: learning to think independently.
+
+Doing something that someone else already knows how to do hauls the world from 1 to n, adding more of what's already known. But when you make something novel, you go from 0 to 1. The next Bill Gates won't construct an operating system. The next Larry Page or Sergey Brin won't create a search engine. Tomorrow's victors won't triumph by being fiercely competitive in today's market. They will elude competition altogether, since their businesses will be one-of-a-kind.
+
+Edit #1: Original Text - "we live in an age of technological stagnation"
+Edited Text - "we exist in an era of technological monotony"
+Suggestion: Consider changing "stagnation" to "monotony" in order to add a more poetic variation.
+
+Edit #2: Original Text - "Progress can be achieved in any industry or area of business"
+Edited Text - "Advancement can be accomplished in any sector or region of industry" 
+Suggestion: Consider changing "industry or area of business" to "sector or region of industry" in order to use more precise language.
+
+Edit #3: Original Text - "The next Bill Gates will not build an operating system" 
+Edited Text - "The next Bill Gates won't construct an operating system" 
+Suggestion: Consider changing "will not build" to "won't construct" in order to make it sound more casual.
+
+Edit #4: Original Text - "will escape" 
+Edited Text - "will elude" 
+Suggestion: Consider changing "escape" to "elude" in order to use more advanced language.
+
+Edit #5: Original Text - "because their businesses will be unique" 
+Edited Text - "since their businesses will be one-of-a-kind" 
+Suggestion: Consider changing "unique" to "one-of-a-kind" in order to make it more vivid. 
+
+END
+
+Prompt: Rewrite this text to ${prompt}. Do not edit quotes. After writing the edit, list each edit made, including the original text, the edited text, and a detailed reason for editing. Write your reasons as suggestions (e.g., "Consider changing <something> in order to make it more interesting). You MUST list EVERY edit made to the text. List EVERY edit made to the text, in order. Do not list edits if nothing changed.
+
+Original Text:
+${text}
+
+Write out the edited text, followed by the list of edits:
+
+Edited Text:`
+
+const generateAction: NextApiHandler = async (req, res) => {
+  if (!req.body.prompt || !req.body.text) {
+    res.status(400).json({ error: "Missing prompt or text" })
+    return
+  }
+
   // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+  console.log(`API: ${basePromptPrefix(req.body.prompt, req.body.text)}`)
 
   const baseCompletion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    prompt: basePromptPrefix(req.body.prompt, req.body.text),
     temperature: 0.7,
-    max_tokens: 250,
+    max_tokens: 1000,
   })
 
   const basePromptOutput = baseCompletion.data.choices.pop()
 
-  res.status(200).json({ output: basePromptOutput })
+  res.status(200).json({
+    output: basePromptOutput,
+    edits: parseEdits(basePromptOutput.text),
+  })
 }
 
 export default generateAction
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,39 @@
 import Head from "next/head"
-import { useState } from "react"
+import { DOMAttributes, useState } from "react"
 import React from "react"
 
 const Home = () => {
-  const [userInput, setUserInput] = useState("")
+  const [text, setText] = useState("")
+  const [textUsed, setTextUsed] = useState("")
+  const [prompt, setPrompt] = useState("")
+  const [apiOutput, setApiOutput] = useState({ edits: [] })
+  const [isGenerating, setIsGenerating] = useState(false)
+  const [selectedEdit, setSelectedEdit] = useState(0)
+
+  const handleSubmit: DOMAttributes<HTMLFormElement>["onSubmit"] = async (
+    e
+  ) => {
+    e.preventDefault()
+    setIsGenerating(true)
+    setTextUsed(text)
+    setSelectedEdit(0)
+
+    console.log("Calling OpenAI...")
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text, prompt }),
+    })
+
+    const data = await response.json()
+    const { output } = data
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(data)
+    setIsGenerating(false)
+  }
 
   return (
     <div className="root">
@@ -12,32 +42,64 @@ const Home = () => {
       </Head>
       <div className="container">
         <div className="mr-auto mb-4">
-          <h1 className="text-4xl font-bold mb-1">Lizoy</h1>
-          <div className="text-lg mt-0.5 text-gray-800">
+          <h1 className="text-4xl font-bold mb-1 manrope">Lizoy</h1>
+          <div className="text-lg mt-0.5 text-gray-800 manrope">
             <h2>Get writing feedback from an expert AI</h2>
           </div>
         </div>
-        <div className="w-full">
+        <form className="w-full" onSubmit={handleSubmit}>
+          <label
+            htmlFor="text-input"
+            className="font-semibold text-gray-700 mb-1 block manrope"
+          >
+            What do you want to improve?
+          </label>
+          <input
+            id="text-input"
+            className="bg-gray-100 rounded-xl p-4 w-full"
+            value={prompt}
+            placeholder="Paste here..."
+            onChange={(e) => setPrompt(e.target.value)}
+          />
           <label
             htmlFor="user-input"
-            className="font-semibold text-gray-700 mb-0.5 block"
+            className="font-semibold text-gray-700 mb-1 block mt-4 manrope"
           >
             Enter text
           </label>
           <textarea
             id="user-input"
             className="bg-gray-100 rounded-xl p-4 w-full min-h-[15rem]"
-            value={userInput}
+            value={text}
             placeholder="Paste here..."
-            onChange={(e) => setUserInput(e.target.value)}
+            onChange={(e) => setText(e.target.value)}
           />
           <div className="prompt-buttons mt-3">
-            <button
-              className="generate-button"
-              onClick={() => alert("generate")}
-            >
-              Get Expert Suggestions
+            <button className="generate-button">
+              {isGenerating ? (
+                <span className="loader"></span>
+              ) : (
+                <p>Get Free Suggestions</p>
+              )}
             </button>
+          </div>
+        </form>
+        <div className="grid grid-cols-[2fr_1fr] gap-6 mt-8">
+          <div className="leading-7 whitespace-pre-wrap">{textUsed}</div>
+          <div>
+            {isGenerating
+              ? "getting high-quality feedback..."
+              : apiOutput.edits.map((edit, i) => (
+                  <div key={i} className="mb-8 p-4 shadow-md border rounded-lg">
+                    <p className="text-sm line-through text-gray-600">
+                      {edit.original}
+                    </p>
+                    <p>{edit.edit}</p>
+                    <p className="text-sm text-gray-800 font-semibold mt-1">
+                      {edit.suggestion}
+                    </p>
+                  </div>
+                ))}
           </div>
         </div>
       </div>
